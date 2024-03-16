ce0c1bdc80b507f3c2cfe4f414af271de1e35ea5
André Luis de Oliveira <65047921+anddreluis2@users.noreply.github.com>
2024-03-15 23:06:36 -0300

    Refactor router initialization

--- router/router.go
+++ router/router.go
@@ -7,11 +7,8 @@ import (
 func Initialize() {
 
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	initializeRoutes(r)
 
 	r.Run(":8080")
 }
