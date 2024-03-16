72993e34b67f07259eee360f37928122127e5cb4
André Luis de Oliveira <65047921+anddreluis2@users.noreply.github.com>
2024-03-15 23:50:17 -0300

    Refactor main.go to initialize database configuration

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/anddreluis2/avenida/router"
+import (
+	"fmt"
+
+	"github.com/anddreluis2/avenida/config"
+	"github.com/anddreluis2/avenida/router"
+)
 
 func main() {
+	//Initialize configs
+
+	err := config.InitDatabase()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	router.Initialize()
 }
