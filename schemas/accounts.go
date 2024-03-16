6c32af2c616c47fbcbec9331cf3551b552fccb55
André Luis de Oliveira <65047921+anddreluis2@users.noreply.github.com>
2024-03-16 10:56:37 -0300

    Update JSON tags in account schemas

--- schemas/accounts.go
+++ schemas/accounts.go
@@ -8,32 +8,32 @@ import (
 
 type Account struct {
 	gorm.Model
-	id             string `gorm:"primaryKey"`
-	clientId       string
-	balance        int64
-	virtualBalance int64
+	id             string `json: "id" gorm: "primaryKey"`
+	clientId       string `json: "clientId"`
+	balance        int64  `json: "balance"`
+	virtualBalance int64  `json: "virtualBalance"`
 }
 
 type Clients struct {
 	gorm.Model
-	id    string `gorm:"primaryKey"`
-	name  string
-	email string
+	id    string `json: "id" gorm: "primaryKey"`
+	name  string `json: "name"`
+	email string `json: "email"`
 }
 
 type Transactions struct {
 	gorm.Model
-	id               string `gorm:"primaryKey"`
-	transaction_type string
-	amount           int64
-	account_id       string
+	id               string `json: "id" gorm: "primaryKey"`
+	transaction_type string `json: "transaction_type"`
+	amount           int64  `json: "amount"`
+	account_id       string `json: "account_id"`
 }
 
 type ScheduledTransactions struct {
 	gorm.Model
-	id               string `gorm:"primaryKey"`
-	transaction_type string
-	amount           int64
-	account_id       string
-	scheduled_for    time.Time
+	id               string    `json: "id" gorm: "primaryKey"`
+	transaction_type string    `json: "transaction_type"`
+	amount           int64     `json: "amount"`
+	account_id       string    `json: "account_id"`
+	scheduled_for    time.Time `json: "scheduled_for"`
 }
