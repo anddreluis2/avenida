15c3df66d65124266b74e4dd66731a34acef08ab
André Luis de Oliveira <65047921+anddreluis2@users.noreply.github.com>
2024-03-15 23:22:56 -0300

    Refactor API routes to use handler functions

--- router/routes.go
+++ router/routes.go
@@ -1,8 +1,7 @@
 package router
 
 import (
-	"net/http"
-
+	"github.com/anddreluis2/avenida/handler"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,39 +9,10 @@ func initializeRoutes(router *gin.Engine) {
 	// group all API routes
 	v1 := router.Group("/api/v1/")
 
-	v1.POST("/deposit", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "Deposit",
-		})
-	})
-
-	v1.POST("/withdraw", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "Withdraw",
-		})
-	})
-
-	v1.POST("/scheduleDeposit", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "Schedule deposit",
-		})
-	})
-
-	v1.POST("/scheduleWithdraw", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "Schedule withdraw",
-		})
-	})
-
-	v1.GET("/getBalance", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "Show balance",
-		})
-	})
-
-	v1.GET("/getVirtualBalance", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "Show virtual balance",
-		})
-	})
+	v1.POST("/deposit", handler.Deposit)
+	v1.POST("/withdraw", handler.Withdraw)
+	v1.POST("/scheduleDeposit", handler.ScheduleDeposit)
+	v1.POST("/scheduleWithdraw", handler.ScheduleWithdraw)
+	v1.GET("/getBalance", handler.GetBalance)
+	v1.GET("/getVirtualBalance", handler.GetVirtualBalance)
 }
